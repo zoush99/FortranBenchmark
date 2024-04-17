30273a3571a483fab3b474aba0668d97efa0ec3f
zoush99 <zoush99@gmail.com>
2024-04-17 21:46:41 +0800

    all

--- civl/abs_bad.f90
+++
@@ -1,9 +0,0 @@
-       program abs
-         implicit none
-!$CVL $input
-         integer :: arg
-         integer :: x
-!$CVL $assume(-10 .LE. arg .AND. arg .LE. 10);
-         x = ABS(arg)
-!$CVL $assert(0 .GE. x);
-       end program abs
--- civl/truncate.f
+++
@@ -1,9 +0,0 @@
-      PROGRAM TRUNCATE
-        IMPLICIT NONE
-        INTEGER extremelylongvariablename, extremelylongvariablename1
-        INTEGER sum
-        extremelylongvariablename = 0
-        extremelylongvariablename1 = 1
-        sum = 10 + extremelylongvariablename + extremelylongvariablename1
-        !$CVL $assert(sum .EQ. 10);
-      END PROGRAM TRUNCATE
